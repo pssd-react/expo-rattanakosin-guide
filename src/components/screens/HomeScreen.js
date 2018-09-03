1fcf276622b4db3a50e5a6224e3373740d1d34fc
prachaya ruangprach <58160124@go.buu.ac.th>
2018-09-03 16:33:39 +0700

    fluke homepage

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,105 +1,155 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
     View,
-    Text, 
+    Text,
     StyleSheet,
     TouchableOpacity,
     ScrollView,
-    Image
+    Image,
+    ImageBackground,
+    Platform,
+    StatusBar,
+    TextInput
 } from 'react-native'
-import { Button  } from '../common/Button';
-import { Card  } from '../common/Card';
+import { Button } from '../common/Button';
+import { Card } from '../common/Card';
 import { CardSection } from '../common/CardSection';
-import { Icon} from 'react-native-elements'
+import { Icon } from 'react-native-elements'
 import axios from 'axios'
 import _ from 'lodash'
 import { HomeMenuScreens } from './homelistscreens'
 import { createStackNavigator } from 'react-navigation'
 
-var  data = {
-        'RqAppID':'1234',
-        'UserLanguage':'EN',
-        'MarketID':'3',
-        'Version':'1.1.4'
-    }
+var data = {
+    'RqAppID': '1234',
+    'UserLanguage': 'EN',
+    'MarketID': '3',
+    'Version': '1.1.4'
+}
 var config = {
-    headers: {  
+    headers: {
         'Authorization': 'Basic Z3Vlc3Q6cGFzc3dvcmQ=',
         'Content-Type': 'application/json'
-            }
+    }
 };
 
 
 
-class HomeScreen extends Component{
-    static navigationOptions = {header: null}
-    state = { 
+class HomeScreen extends Component {
+    static navigationOptions = { header: null }
+    state = {
         item: ''
     };
 
-    componentWillMount(){
+    componentWillMount() {
+        this.startHeaderHeight = 80
+        if (Platform.OS == 'android') {
+            this.startHeaderHeight = 100 + StatusBar.currentHeight
+        }
+
         axios.post('https://uat-shop.digitalventures.co.th/wp-json/jj/dvservice/v1/InquiryMenuGuideService',
-        data , config )
-        .then(response => {this.setState({ item : response.data } ) })
-        .catch((error) => {
-            console.log('axios error:',error);
-        });
-       
-    }
-    
-    renderItem(){
-       //console.log(this.state.item)
-            const hellow =  _.map((this.state), (items) => {
-                return (<ItemDetail items={ items.MenuList } />)
-            })
-        
-        return  hellow 
+            data, config)
+            .then(response => { this.setState({ item: response.data }) })
+            .catch((error) => {
+                console.log('axios error:', error);
+            });
+
     }
 
+    renderItem() {
+        //console.log(this.state.item)
+        const hellow = _.map((this.state), (items) => {
+            return (<ItemDetail items={items.MenuList} />)
+        })
+
+        return hellow
+    }
 
 
-  render(){
+
+    render() {
         //console.log(this.state)
         return (
-            <Card style={{flex:1}}>
+            <View style={{ flex: 1 }}>
                 <ScrollView>
-                <CardSection>
-                    <View style={{ flexDirection: 'row' ,  marginLeft: 60  }} >
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image
-                                style={{ flexDirection: 'column', marginLeft: 10 }}
-                                source={ require('../images/drawable-hdpi/ic_type_category_food.webp')}
+                    <ImageBackground
+                        style={{ height: this.startHeaderHeight, borderBottomWidth: 1, borderBottomColor: '#dddddd', width: '100%', height: 200 }}
+                        source={require('../assets/Wat-Suthat.jpg')}
+                    >
+
+                        <View style={{
+                            flexDirection: 'row', padding: 10,
+                            backgroundColor: 'white', marginHorizontal: 20,
+                            shadowOffset: { width: 0, height: 0 },
+                            shadowColor: 'black',
+                            shadowOpacity: 0.2,
+                            elevation: 1,
+                            marginTop: Platform.OS == 'android' ? 30 : null
+                        }}>
+                            <Icon name="search" size={20} style={{ marginRight: 10 }} />
+                            <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="ค้นหา ..."
+                                placeholderTextColor="grey"
+                                style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
                             />
+
                         </View>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image
-                                style={{ flexDirection: 'column', marginLeft: 10 }}
-                                source={ require('../images/drawable-hdpi/ic_type_category_bank.webp')}
-                            />
+
+                    </ImageBackground>
+                    <CardSection style={{ bottom: 50, marginLeft: '4%', marginRight: '4%' }}>
+                        <View style={{ flexDirection: 'row' }} >
+                            <View style={styles.imgLeft}>
+                                <Image
+                                    style={{ width: '100%', height: '100%' }}
+                                    source={require('../images/drawable-hdpi/ic_main_food.webp')}
+                                />
+                                <Text style={styles.textLeft} >
+                                    กิน
+                                </Text>
+                            </View>
+                            <View style={styles.imgRight}>
+                                <Image
+                                    style={{ width: '100%', height: '100%' }}
+                                    source={require('../images/drawable-hdpi/ic_main_travel.webp')}
+                                />
+                                <Text style={styles.textLeft} >
+                                    เที่ยว
+                                </Text>
+                            </View>
+
+                            <View style={styles.imgRight}>
+                                <Image
+                                    style={{ width: '100%', height: '100%' }}
+                                    source={require('../images/drawable-hdpi/ic_main_shop.webp')}
+                                />
+                                <Text style={styles.textLeft} >
+                                    ช้อป
+                                </Text>
+                            </View>
+                            <View style={styles.imgRight}>
+                                <Image
+                                    style={{ width: '100%', height: '100%' }}
+                                    source={require('../images/drawable-hdpi/ic_main_other.webp')}
+                                />
+                                <Text style={styles.textLeft} >
+                                    อื่นๆ
+                                </Text>
+                            </View>
+
                         </View>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image
-                                style={{ flexDirection: 'column', marginLeft: 10 }}
-                                source={ require('../images/drawable-hdpi/ic_type_category_facilities.webp')}
-                            />
-                        
-                        </View>
-                        <TouchableOpacity onPress={()=>this.props.navigation.navigate('Menu')}>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image
-                                style={{ flexDirection: 'column', marginLeft: 10 }}
-                                source={ require('../images/drawable-hdpi/ic_type_category_hotel.webp')}
-                            />
-                        </View>
-                        </TouchableOpacity>
-                     </View>
-                </CardSection>
-                <CardSection>
-                    {this.renderItem()}
-                </CardSection>
+
+                    </CardSection>
+
+                    <Card style={{ flex: 1 }}>
+                        <ScrollView>
+                            <CardSection>
+                                {this.renderItem()}
+                            </CardSection>
+                        </ScrollView>
+                    </Card>
                 </ScrollView>
-            </Card>
-            
+            </View>
         );
     }
 
@@ -117,7 +167,7 @@ const styles = StyleSheet.create({
     headerTextStyle: {
         fontSize: 18
     },
-    thumbnailStyle:{
+    thumbnailStyle: {
         height: 50,
         width: 50
     },
@@ -131,7 +181,45 @@ const styles = StyleSheet.create({
         height: 300,
         width: '100%',
         flex: 1
-    }
+    }, imgLeft: {
+        flexDirection: 'column',
+        marginRight: 20,
+        marginTop: 20,
+        marginBottom: 5,
+        width: 70,
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, imgRight: {
+        flexDirection: 'column',
+        marginLeft: 10,
+        marginRight: 20,
+        marginTop: 20,
+        marginBottom: 5,
+        width: 70,
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textLeft: {
+        marginLeft: 50,
+        marginTop: 10,
+        width: '100%',
+        fontSize: 12,
+    },
+    imgCenter: {
+        flexDirection: 'column',
+        marginLeft: 40,
+        marginRight: 45,
+        marginTop: 20,
+        marginBottom: 5,
+        width: 70,
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+
 })
 
 const {
@@ -145,40 +233,40 @@ const {
 
 class ItemDetail extends Component {
     state = {
-        item : []
+        item: []
     }
-  
-    renderImg(imgs){
+
+    renderImg(imgs) {
         //if scale blah blah
         const base_url = 'https://uat-shop.digitalventures.co.th'
 
-            return _.map(imgs.SliderList, imgSlider=>{
-               return (<Image
-                        style={{width: 150, height: 100, borderRightWidth: 5}}
-                        source={{uri: 'https://images-na.ssl-images-amazon.com/images/I/51Gji7jFNjL._SX425_.jpg'}}
-                        />)
-            })
- 
+        return _.map(imgs.SliderList, imgSlider => {
+            return (<Image
+                style={{ width: 150, height: 100, borderRightWidth: 5 }}
+                source={{ uri: 'https://images-na.ssl-images-amazon.com/images/I/51Gji7jFNjL._SX425_.jpg' }}
+            />)
+        })
+
     }
 
-    renderData(){
-        return _.map(this.props.items, item =>{
+    renderData() {
+        return _.map(this.props.items, item => {
             return (
-                <Card  style={{ flex: 4}}>
-                    <CardSection  style={{ flex: 1}}>
-                    <View style={headerContentStyle}>
-                        <CardSection style={{flexDirection:'row', flex:1}}>
-                        <Text style={headerTextStyle} >{item.Name}</Text>
-                        <Text style={{justifyContent:'flex-end'}} >ดูทั้งหมด</Text>
-                        </CardSection>
-                    </View>
+                <Card style={{ flex: 4 }}>
+                    <CardSection style={{ flex: 1 }}>
+                        <View style={headerContentStyle}>
+                            <CardSection style={{ flexDirection: 'row', flex: 1 }}>
+                                <Text style={headerTextStyle} >{item.Name}</Text>
+                                <Text style={{ justifyContent: 'flex-end' }} >ดูทั้งหมด</Text>
+                            </CardSection>
+                        </View>
                     </CardSection>
                     <CardSection >
                         <ScrollView
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
                         >
-                        {this.renderImg(item)}
+                            {this.renderImg(item)}
                         </ScrollView>
                     </CardSection>
                 </Card>
@@ -186,7 +274,7 @@ class ItemDetail extends Component {
         })
     }
 
-    render(){
+    render() {
         return (
             <View>
                 {this.renderData()}
@@ -200,11 +288,11 @@ class ItemDetail extends Component {
 
 
 const HomeMenu = createStackNavigator({
-    Main : {
-        screen : HomeScreen
+    Main: {
+        screen: HomeScreen
     },
-    Menu : {
-        screen : HomeMenuScreens
+    Menu: {
+        screen: HomeMenuScreens
     },
 })
 
