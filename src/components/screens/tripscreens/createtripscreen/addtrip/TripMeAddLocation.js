71daa7b1f604b531e91f7252a779ff6f78cf5421
Hanchai158 <36034930+Hanchai158@users.noreply.github.com>
2018-11-01 11:06:45 +0700

    Delete TripMe

--- src/components/screens/tripscreens/createtripscreen/addtrip/TripMeAddLocation.js
+++ src/components/screens/tripscreens/createtripscreen/addtrip/TripMeAddLocation.js
@@ -7,17 +7,17 @@ import {
     ImageBackground,
     Dimensions,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Alert
 } from 'react-native'
 import _ from 'lodash'
 import axios from 'axios'
 import { ButtonTrip } from '../../../../common/ButtonTrip'
 import { createStackNavigator } from 'react-navigation'
 import { HeaderBackButton } from 'react-navigation'
-import  Search  from './Search'
-import { ResultSearchLocation } from './ResultSearchLocation'
-import { AddTripScreen } from './AddTripScreen'
 import I18n from '../../../../config/i18n'
+import Swipeout from 'react-native-swipeout';
 import { ButtonStar,ButtonLocal, CardSection, Header, Button, ModalSpinner } from '../../../../common'
 
 const ImgWidth = Dimensions.get('window').width
@@ -214,75 +214,129 @@ export class TripMeAddLocation extends Component {
         })
     }
 
+    deleteNote(ShopID, TripID ){
+    
+        const data = {
+            "RqAppID":"1234",
+            "UserLanguage":I18n.t('serviceLang'),
+            "UserID":this.props.screenProps.userId,
+            "TripID":TripID,
+            "TripShop":[{
+                    "ShopID":ShopID
+                }],
+            "SessionToken":"",
+            "MarketID":"3"
+        }
+        const config = {
+            headers: {
+                'Authorization' : 'Basic Z3Vlc3Q6cGFzc3dvcmQ=',
+                'Content-Type' : 'application/json'
+            }
+        }
+        this.setState({loading : true})
+        axios.post('https://uat-shop.digitalventures.co.th/wp-json/jj/dvservice/v1/DeleteTripShopService',
+        data, config)
+        .then(Response => {  
+            if(Response.data.ResponseDetail === "Success"){
+                this.setState({loading : false})
+            }else{
+                this.setState({loading : false})
+            }
+        })
+        .catch((error) => {
+            console.log('axios error:', error)
+            this.setState({loading : false})
+        });
+
+    }
+
     _renderListTrip(){
         let renderingItem = []
+        let triId
         let i=0
          _.map(this.state.items.Trip, items => {
             if(items.TripType === "01" && items.TripShop.length > 0){
                 _.map(items.TripShop, TripShop => {
-                    renderingItem[i] = TripShop.ShopID
+                    renderingItem[i] = items.TripID+'_'+TripShop.ShopID
                     i++
         })
             }
         })
-
-        if(renderingItem.length !== 0){
-            return _.map(this.state.itemShop, items => {
-                return _.map(items, item => {
-                    for (let i=0; i<renderingItem.length; i++){
-                       // return <Text>{renderingItem[i]}</Text>
-                      if(renderingItem[i] === item.ShopID){
-                            return (
-                                <TouchableOpacity onPress={() => this.onImgSlidePress(item.ShopID)}>
-                                    <View key={item.CategoryName+'_'+item.ShopID} style={{flex:1}}>
-                                        <CardSection style={{flex:2}}>   
-                                        <View style={{flex:1, flexDirection: 'column', backgroundColor: "#FFFAFA", padding:10}}>
-                                            <View style={styles.ViewContainer}>
-                                                <View style={{flex: 1  }}>
-                                                    <Image style={{width:120, height:120}}
-                                                        source={(item.ImageUrl === "")? require('../../../../images/drawable-hdpi/placeholder_merchant_item.webp'):{uri: item.ImageUrl} } /> 
-                                                </View>
-                                                <View style={{ flex: 2 ,flexDirection: 'column', marginLeft: 15 }}>
-                                                    <View style= {{ flexDirection: 'row' }}>
-                                                        <Text style={styles.ViewTextStyle} numberOfLines={1}> {item.LocationName} </Text>
-                                                    </View>
-                                                    <View style= {{ flexDirection: 'row' , height: 40}}>
-                                                        {this.renderIconShopCategory(item.ShopCategory)}
-                                                    </View>
-                                                    <View style= {{ flexDirection: 'row' , height: 40}}>
-                                                        <View style={{ flex: 1 , marginRight: 15} }>
-                                                            <ButtonStar style={styles.buttonStarStyle}> 
-                                                            {item.Rating}
-                                                            </ButtonStar>
-                                                        </View>
-                                                        <View  style={{ flex: 3}}/>
-                                                    </View>
-                                                </View>
-                                            </View>
-                                            </View>
-                                        </CardSection>
-                                    </View> 
-                                </TouchableOpacity>
-                            )
-                        }
-                    }
-                })
-            })
-        }else{
+        if(this.state.loading === true){
             return (
-                <View style={{flex: 1, flexDirection:"column", alignItems: "center", justifyContent: "center", marginTop: 20}}>
-                    <View style={{ flex:1, alignItems: "center", flexDirection:"row", marginRight:40 }}>
-                        <Image style={{ width:60, height:80}}
-                            source={require('../../../../images/drawable-xhdpi/ic_arrow_no_trip.webp')}/> 
-                        <Text style={{color: "gray", marginTop:40 }} >{I18n.t('tripNotInteresting')}</Text>
-                    </View>
-                    <View style={{ flex:1, alignItems: "center"}}>
-                       
-                        <Text style={{color: "gray" }} >{I18n.t('needAddNotStore')}</Text>
-                        <Text style={{color: "gray" }} >{I18n.t('needAddNotStore01')}</Text>
-                    </View>
-                </View>
+                <ModalSpinner />
             )
+        }else{
+            if(renderingItem.length !== 0){
+                return _.map(this.state.itemShop, items => {
+                    return _.map(items, item => {
+                        for (let i=0; i<renderingItem.length; i++){
+                            let split= renderingItem[i].split('_')
+                            let tripId=split[0]
+                            let shopId=split[1]
+                            if(shopId === item.ShopID){
+                                return (
+                                <Swipeout right={[{
+                                    text: 'Delete',
+                                    backgroundColor: 'red',
+                                    underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+                                    onPress: () => { this.deleteNote(shopId, tripId) }
+                                }]}
+                                autoClose='true'
+                                backgroundColor= 'transparent'>
+    
+                                    <TouchableWithoutFeedback onPress={() => this.onImgSlidePress(item.ShopID)}>
+                                        <View key={item.CategoryName+'_'+item.ShopID} style={{flex:1}}>
+                                            <CardSection style={{flex:2}}>   
+                                            <View style={{flex:1, flexDirection: 'column', backgroundColor: "#FFFAFA", padding:10}}>
+                                                <View style={styles.ViewContainer}>
+                                                    <View style={{flex: 1  }}>
+                                                        <Image style={{width:120, height:120}}
+                                                            source={(item.ImageUrl === "")? require('../../../../images/drawable-hdpi/placeholder_merchant_item.webp'):{uri: item.ImageUrl} } /> 
+                                                    </View>
+                                                    <View style={{ flex: 2 ,flexDirection: 'column', marginLeft: 15 }}>
+                                                        <View style= {{ flexDirection: 'row' }}>
+                                                            <Text style={styles.ViewTextStyle} numberOfLines={1}> {item.LocationName} </Text>
+                                                        </View>
+                                                        <View style= {{ flexDirection: 'row' , height: 40}}>
+                                                            {this.renderIconShopCategory(item.ShopCategory)}
+                                                        </View>
+                                                        <View style= {{ flexDirection: 'row' , height: 40}}>
+                                                            <View style={{ flex: 1 , marginRight: 15} }>
+                                                                <ButtonStar style={styles.buttonStarStyle}> 
+                                                                {item.Rating}
+                                                                </ButtonStar>
+                                                            </View>
+                                                            <View  style={{ flex: 3}}/>
+                                                        </View>
+                                                    </View>
+                                                </View>
+                                                </View>
+                                            </CardSection>
+                                        </View> 
+                                    </TouchableWithoutFeedback>
+                                </Swipeout>
+                                )
+                            }
+                        }
+                    })
+                })
+            }else{
+                return (
+                    <View style={{flex: 1, flexDirection:"column", alignItems: "center", justifyContent: "center", marginTop: 20}}>
+                        <View style={{ flex:1, alignItems: "center", flexDirection:"row", marginRight:40 }}>
+                            <Image style={{ width:60, height:80}}
+                                source={require('../../../../images/drawable-xhdpi/ic_arrow_no_trip.webp')}/> 
+                            <Text style={{color: "gray", marginTop:40 }} >{I18n.t('tripNotInteresting')}</Text>
+                        </View>
+                        <View style={{ flex:1, alignItems: "center"}}>
+                           
+                            <Text style={{color: "gray" }} >{I18n.t('needAddNotStore')}</Text>
+                            <Text style={{color: "gray" }} >{I18n.t('needAddNotStore01')}</Text>
+                        </View>
+                    </View>
+                )
+            }
         }
     }
 
